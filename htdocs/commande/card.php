9421742e89448dee792dcddbbb2d3d93f170c0e1
Luciano Yomayel <l.yomayel@gmail.com>
2023-12-10 21:22:57 +1100

    sacamos el comentario al select warehouse

--- htdocs/commande/card.php
+++ htdocs/commande/card.php
@@ -2194,7 +2194,7 @@ if ($action == 'create' && $usercancreate) {
 					// 'text' => $langs->trans("ConfirmClone"),
 					// array('type' => 'checkbox', 'name' => 'clone_content', 'label' => $langs->trans("CloneMainAttributes"), 'value' => 1),
 					// array('type' => 'checkbox', 'name' => 'update_prices', 'label' => $langs->trans("PuttingPricesUpToDate"), 'value' => 1),
-					// array('type' => 'other', 'name' => 'idwarehouse', 'label' => $langs->trans("SelectWarehouseForStockDecrease"), 'value' => $formproduct->selectWarehouses(GETPOST('idwarehouse', 'int') ?GETPOST('idwarehouse', 'int') : 'ifone', 'idwarehouse', '', 1, 0, 0, '', 0, $forcecombo))
+					array('type' => 'other', 'name' => 'idwarehouse', 'label' => $langs->trans("SelectWarehouseForStockDecrease"), 'value' => $formproduct->selectWarehouses(GETPOST('idwarehouse', 'int') ?GETPOST('idwarehouse', 'int') : 'ifone', 'idwarehouse', '', 1, 0, 0, '', 0, $forcecombo))
 				);
 			}
 
@@ -2321,14 +2321,7 @@ if ($action == 'create' && $usercancreate) {
 			}
 
 			if (!$error) {
-				?>
-				<script type="text/javascript">
-					console.log(json_encode($formquestion))
-				</script>
-				<?php
-
-echo '<script>console.log(' . json_encode($formquestion) . ');</script>';
-
+				echo '<script>console.log(' . json_encode($formquestion) . ');</script>';
 				$formconfirm = $form->formconfirm($_SERVER["PHP_SELF"].'?id='.$object->id, $langs->trans('ValidateOrder'), $text, 'confirm_validate', $formquestion, 0, 1, 220);
 			}
 		}
