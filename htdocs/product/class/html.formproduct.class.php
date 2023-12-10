a65bacf59a140da96be640f512c67ef074536bba
Luciano Yomayel <l.yomayel@gmail.com>
2023-12-11 07:10:25 +1100

    select warehouses by id pedido

--- htdocs/product/class/html.formproduct.class.php
+++ htdocs/product/class/html.formproduct.class.php
@@ -218,7 +218,7 @@ class FormProduct
 			$objUser = $this->db->fetch_object($resqlUser);
 			$idUser = $objUser->fk_user_author;
 		}
-
+		echo "idUser: ".$idUser;
 		$sql = "SELECT e.rowid, e.ref as label, e.description, e.fk_parent";
 		if (!empty($fk_product) && $fk_product > 0) {
 			if (!empty($batch)) {
@@ -231,7 +231,7 @@ class FormProduct
 		}
 		$sql .= " FROM ".$this->db->prefix()."entrepot as e";
 		$sql .= " LEFT JOIN ".$this->db->prefix()."product_stock as ps on ps.fk_entrepot = e.rowid";
-		$sql .= " LEFT JOIN ".$this->db->prefix()."user_warehouse_restrictions as uwr ON e.rowid = uwr.entrepot_id";
+		// $sql .= " LEFT JOIN ".$this->db->prefix()."user_warehouse_restrictions as uwr ON e.rowid = uwr.entrepot_id";
 		if (!empty($fk_product) && $fk_product > 0) {
 			$sql .= " AND ps.fk_product = ".((int) $fk_product);
 			if (!empty($batch)) {
@@ -239,7 +239,7 @@ class FormProduct
 			}
 		}
 		$sql .= " WHERE e.entity IN (".getEntity('stock').")";
-		$sql .= " AND uwr.user_id = " . $idUser;
+		// $sql .= " AND uwr.user_id = " . $idUser;
 		if (count($warehouseStatus)) {
 			$sql .= " AND e.statut IN (".$this->db->sanitize(implode(',', $warehouseStatus)).")";
 		} else {
