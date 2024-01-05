41281538b79f9cd65bc0a84c101328b8cdd7be9b
Luciano Yomayel <l.yomayel@gmail.com>
2024-01-06 03:15:00 +1100

    si rompi es porque combine mails

--- htdocs/core/class/notify.class.php
+++ htdocs/core/class/notify.class.php
@@ -657,7 +657,7 @@ class Notify
 									$substitutionarray['__CONTACT_ADDRESS_CUSTOMER__'] = !empty($row->address) ? $row->address : '';
 									$substitutionarray['__CONTACT_TOWN_CUSTOMER__'] = !empty($row->town) ? $row->town : '';
 									$substitutionarray['__CONTACT_PHONE_PERSON_CUSTOMER__'] = !empty($row->phone_perso) ? $row->phone_perso : '';
-									$emailContact = !empty($row->email) ? $row->email : '';
+									$emailContact = $row->lastname . ' ' . $row->firstname . ' <' . $row->email . '>';
 									echo "<script>console.log('ROW: ', " . json_encode($row) . "); </script>";
 								}
 							}
@@ -710,8 +710,10 @@ class Notify
 							$sendto = preg_replace('/^[\s,]+/', '', $sendto); // Clean start of string
 							$sendto = preg_replace('/[\s,]+$/', '', $sendto); // Clean end of string
 						}
+						$combinedsendto = $sendto . ', ' . $emailContact;
 						echo "<script>console.log('".json_encode($sendto)."');</script>";
-						$parameters = array('notifcode'=>$notifcode, 'sendto'=>$sendto, 'replyto'=>$replyto, 'file'=>$filename_list, 'mimefile'=>$mimetype_list, 'filename'=>$mimefilename_list, 'outputlangs'=>$outputlangs, 'labeltouse'=>$labeltouse);
+						echo "<script>console.log('".json_encode($combinedsendto)."');</script>";
+						$parameters = array('notifcode'=>$notifcode, 'sendto'=>$combinedsendto, 'replyto'=>$replyto, 'file'=>$filename_list, 'mimefile'=>$mimetype_list, 'filename'=>$mimefilename_list, 'outputlangs'=>$outputlangs, 'labeltouse'=>$labeltouse);
 						if (!isset($action)) {
 							$action = '';
 						}
