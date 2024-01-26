3e68f1d562074078fa99a2af97b2a1730044da54
Luciano Yomayel <l.yomayel@gmail.com>
2024-01-26 18:33:06 +1100

    ahora bien el console log

--- htdocs/product/list.php
+++ htdocs/product/list.php
@@ -1224,7 +1224,7 @@ include DOL_DOCUMENT_ROOT.'/core/tpl/extrafields_list_search_input.tpl.php';
 $parameters = array('arrayfields'=>$arrayfields);
 $reshook = $hookmanager->executeHooks('printFieldListOption', $parameters, $object, $action); // Note that $action and $object may have been modified by hook
 print $hookmanager->resPrint;
-echo '<script> console.log("'.$arrayfields.'"); </script>';
+echo '<script> console.log('.json_encode($arrayfields).'); </script>';
 // Date creation
 if (!empty($arrayfields['p.datec']['checked'])) {
 	print '<td class="liste_titre">';
